c7faf5c54044028e283e84e54b35d98db34f7243
builtbyjosh <admin@builtbyjosh.com>
2020-08-17 13:44:45 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,68 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song  
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    #song.save
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    result = nil
+    result = @@all.detect{|n| n.name == name}
+    result
+  end
+
+  def self.find_or_create_by_name(name)    
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    new_file = filename.split(" - ")
+    artist_name = new_file[0]
+    song_name = new_file[1].rpartition('.').first
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    new_file = filename.split(" - ")
+    artist_name = new_file[0]
+    song_name = new_file[1].rpartition('.').first
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 end
+
